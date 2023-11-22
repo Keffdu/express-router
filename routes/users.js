72937e36cfd6da6ed5484c9a14ed5d0628aeac25
Kevin Dutzy <kddutz15@gmail.com>
2023-11-22 14:59:11 -0700

    created user router, moving to personal to see if i can avoid errors with localhost

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const { Router } = require("express")
+const { User } = require("../models/index")
+const user = Router()
+
+user.get("/", async (req, res) => {
+    const allUsers = await User.findAll()
+    res.json(allUsers)
+})
+
+user.get("/:id", async (req, res) => {
+    const user = await User.findByPk(req.params.id)
+    res.json(user)
+})
+
+user.post("/", async (req, res) => {
+    const user = await User.create(req.body)
+    res.json(user)
+})
+
+user.put("/id", async (req, res) => {
+    const user = await User.findByPk(req.params.id)
+    await user.update(req.body)
+    res.json(user)
+})
+
+user.delete("/:id", async (req, res) => {
+    const user = await User.findByPk(req.params.id)
+    await user.destroy()
+    res.send(res.statusCode)
+})
+
+module.exports = user;
